dc8ea1afd5518513cb7129d70adead883b765cd0
Mayursinh <marvinduff97@gmail.com>
2019-12-31 23:12:45 +0530

    structured output on terminal

--- bin/structurize.js
+++ bin/structurize.js
@@ -3,6 +3,7 @@ const yaml = require('yaml');
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const logSymbols = require('log-symbols');
 const Process = require('../lib/Process');
 
 const currPath = process.cwd();
@@ -15,20 +16,19 @@ fs.readFile(yamlFilePath, 'utf8', (err, structure) => {
   const P1 = new Process(parsedStructure, currPath);
 
   try {
+    console.log(`\n${chalk.bold('ROOT : ')}${currPath}\n`);
     const report = P1.generateReport();
     const { positive, negative, neutral } = report;
 
+    console.log('\n');
     if (positive.number) {
-      console.log(`\n${chalk.blue(positive.number)} file(s)/folder(s) exists as well as defined in structure.yml`);
-      console.log(`\n${JSON.stringify(positive.detailsOfChunks, null, ' ')}`);
+      console.log(logSymbols.success, `${chalk.green(positive.number)} file(s)/folder(s) exists as well as defined`);
     }
     if (negative.number) {
-      console.log(`\n${chalk.red(negative.number)} file(s)/folder(s) don't exist but defined in structure.yml`);
-      console.log(`\n${JSON.stringify(negative.detailsOfChunks, null, ' ')}`);
+      console.log(logSymbols.error, `${chalk.red(negative.number)} file(s)/folder(s) don't exist but defined`);
     }
     if (neutral.number) {
-      console.log(`\n${chalk.green(neutral.number)} file(s)/folder(s) exist but not defined in structure.yml`);
-      console.log(`\n${JSON.stringify(neutral.detailsOfChunks, null, ' ')}`);
+      console.log(logSymbols.warning, `${chalk.cyan(neutral.number)} file(s)/folder(s) exist but not defined`);
     }
   } catch (error) {
     throw new Error(error);
